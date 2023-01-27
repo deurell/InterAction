1eb3a85cfde2f0fc82b64e3eada6781bdfa94550
Mikael Deurell <mikael@deurell.net>
2023-01-27 15:09:00 +0100

    rotation fix

--- InterAction/InterActionSystem.swift
+++ InterAction/InterActionSystem.swift
@@ -18,7 +18,7 @@ class InterActionSystem : RealityKit.System
         if let grabbedEntity = context.scene.performQuery(GrabbedComponent.query).map({ $0 }).first {
             guard let component = grabbedEntity.components[GrabbedComponent.self] as? GrabbedComponent else { return }
             let moveVector = camera.transform.translation - component.startCameraPosition
-            let panOffsetScale: Float = 0.0004
+            let panOffsetScale: Float = 0.0006
             let offsetVector = component.panOffset * panOffsetScale
             
             let cameraForwardVector = camera.transform.matrix.forward
@@ -28,10 +28,9 @@ class InterActionSystem : RealityKit.System
             
             grabbedEntity.transform.translation = component.startEntityPosition + moveVector + cameraAdjustedOffsetVector
             grabbedEntity.components.set(component)
-            
-            let perpendicularCameraForward = simd_cross([0,1,0], cameraForwardVector)
-            
-            grabbedEntity.transform.rotation = simd_quatf(angle: -.pi/3, axis: perpendicularCameraForward) *  simd_quatf(angle: -.pi/2, axis: cameraForwardVector) * camera.transform.rotation
+                        
+            grabbedEntity.transform.rotation =  camera.transform.rotation * simd_quatf(angle: Float.pi/2, axis: [0,0,1]) *  simd_quatf(angle: Float.pi/4, axis: [1,0,0])
+
             
             updateCrosshair(camera: camera, crosshair: crosshair, entity: grabbedEntity)
         } else {
@@ -65,5 +64,9 @@ class InterActionSystem : RealityKit.System
 extension float4x4 {
     var forward: SIMD3<Float> {
         normalize(SIMD3<Float>(-columns.2.x, -columns.2.y, -columns.2.z))
+    }
+        
+    var right: SIMD3<Float> {
+        normalize(SIMD3<Float>(columns.0.x, columns.0.y, columns.0.z))
     }
 }
